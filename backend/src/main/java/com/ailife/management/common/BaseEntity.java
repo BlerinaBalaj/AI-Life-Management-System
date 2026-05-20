b28605bbee5c20f00807dbebef952f584a3900e2
BlerinaBalaj <blerina.balaj1@student.uni-pr.edu>
2026-05-20 13:27:53 +0200

    add base entity and custom bad request exception

---
+++ backend/src/main/java/com/ailife/management/common/BaseEntity.java
@@ -0,0 +1,27 @@
+package com.ailife.management.common;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import java.time.Instant;
+
+@Getter
+@Setter
+@MappedSuperclass
+public abstract class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreationTimestamp
+    private Instant createdAt;
+
+    @UpdateTimestamp
+    private Instant updatedAt;
+}
